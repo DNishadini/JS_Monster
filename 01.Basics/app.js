decbcae2dc864db85140e54ae29e3ece1dc2c946
DNishadini <dinushikaherath150@gmail.com>
2026-01-21 01:55:04 +0530

    JS Boolean

--- 01.Basics/app.js
+++ 01.Basics/app.js
@@ -1,6 +1,7 @@
 //clg -> console.log()
 //log -> console.log()
 
+//Example 1
 /*let name = "Dinushika";
 let whatDoYouWannaBecomeInYourLife = "Full Stack Developer";
 let gender = "Female";
@@ -8,7 +9,9 @@ console.log(name);
 console.log(whatDoYouWannaBecomeInYourLife);
 console.log(gender);
 */
-let firstFavNum = 7;
+
+//Example 2
+/*let firstFavNum = 7;
 let secondFavNum = 3.5;
 console.log(firstFavNum + secondFavNum);
 console.log(firstFavNum - secondFavNum);
@@ -16,3 +19,12 @@ console.log(firstFavNum * secondFavNum);
 console.log(firstFavNum / secondFavNum);
 console.log(firstFavNum % secondFavNum);
 console.log(firstFavNum ** secondFavNum);
+*/
+
+//Example 3
+let isJsProgrammingLanguage = true;
+let isJsHard = false;
+let favNumber = 7;
+console.log(isJsProgrammingLanguage);
+console.log(isJsHard);
+console.log(favNumber + undefined);
