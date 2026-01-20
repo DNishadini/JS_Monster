6e0134f9761e8d19760e0cb0e023e2e6c464046d
DNishadini <dinushikaherath150@gmail.com>
2026-01-20 23:06:49 +0530

    JS variables

---
+++ 01.Basics/app.js
@@ -0,0 +1,9 @@
+//clg -> console.log()
+//log -> console.log()
+
+let name = "Dinushika";
+let whatDoYouWannaBecomeInYourLife = "Full Stack Developer";
+let gender = "Female";
+console.log(name);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
