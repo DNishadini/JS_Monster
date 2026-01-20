0a295413e6ada5fc92609a3e2335be0a4ebcb069
DNishadini <dinushikaherath150@gmail.com>
2026-01-21 01:22:51 +0530

    JS basic number operations

--- 01.Basics/app.js
+++ 01.Basics/app.js
@@ -1,9 +1,18 @@
 //clg -> console.log()
 //log -> console.log()
 
-let name = "Dinushika";
+/*let name = "Dinushika";
 let whatDoYouWannaBecomeInYourLife = "Full Stack Developer";
 let gender = "Female";
 console.log(name);
 console.log(whatDoYouWannaBecomeInYourLife);
 console.log(gender);
+*/
+let firstFavNum = 7;
+let secondFavNum = 3.5;
+console.log(firstFavNum + secondFavNum);
+console.log(firstFavNum - secondFavNum);
+console.log(firstFavNum * secondFavNum);
+console.log(firstFavNum / secondFavNum);
+console.log(firstFavNum % secondFavNum);
+console.log(firstFavNum ** secondFavNum);
