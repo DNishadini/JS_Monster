710b3817ccc675a16f7af7f2bddd3427ba4a27e3
DNishadini <dinushikaherath150@gmail.com>
2026-01-21 11:25:23 +0530

    JS strings

--- 01.Basics/app.js
+++ 01.Basics/app.js
@@ -30,7 +30,7 @@ console.log(isJsHard);
 console.log(favNumber + undefined);*/
 
 //Example 4
-let firstFavNum = 7;
+/*let firstFavNum = 7;
 let secondFavNum = 3.5;
 console.log(firstFavNum > secondFavNum);
 console.log(firstFavNum < secondFavNum);
@@ -39,4 +39,17 @@ console.log(firstFavNum <= secondFavNum);
 console.log(firstFavNum === secondFavNum);
 console.log(firstFavNum == secondFavNum);
 console.log(firstFavNum !== secondFavNum);
-console.log(firstFavNum != secondFavNum);
+console.log(firstFavNum != secondFavNum);*/
+
+//Example 5
+let favActorFirstName = "Robert";
+let favActorLastName = "Downey Jr.";
+let fullName = favActorFirstName.concat(favActorLastName);
+console.log(fullName);
+let uppercase = fullName.toUpperCase();
+console.log(uppercase);
+let message = `My favorite actor is ${uppercase} and say something about your fav actor`;
+console.log(message);
+
+message += ". He is the best actor in the world.";
+console.log(message);
