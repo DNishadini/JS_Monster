316a21e18bfd556924522919bf9c3b7c769705fe
DNishadini <dinushikaherath150@gmail.com>
2026-01-21 12:41:54 +0530

    JS conditional Statemetnt(IF)

---
+++ 02.Control Flow/app.js
@@ -0,0 +1,11 @@
+//Example 01
+let password = 8;
+if (password === 8) {
+  console.log("Welcome");
+} else if (password <= 8) {
+  console.log("Password is too short");
+} else if (password >= 8) {
+  console.log("Password is too long");
+} else {
+  console.log("Invalid password");
+}
