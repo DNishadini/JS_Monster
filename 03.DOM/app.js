77a75fb7bed9c95c9d57176023c523cf7650f145
DNishadini <dinushikaherath150@gmail.com>
2026-01-24 13:04:21 +0530

    Accessnig the DOM

---
+++ 03.DOM/app.js
@@ -0,0 +1,15 @@
+//Example 1: Accessing DOM Elements
+const h4 = document.getElementsByTagName("h4");
+//console.log(h4);
+
+const green = document.getElementsByClassName("green");
+//console.log(green);
+
+const blue = document.getElementById("blue");
+console.log(blue);
+
+const yellow = document.querySelector("#yellow");
+console.log(yellow);
+
+const allTeal = document.querySelectorAll(".teal");
+console.log(allTeal);
